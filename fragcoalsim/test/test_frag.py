a53b8a92ee5cef1a1b317101c7ada918de85ae83
Jamie R. Oaks <joaks1@gmail.com>
2017-11-03 19:37:35 -0500

    Adding test of FragmentationModel.

--- fragcoalsim/test/test_frag.py
+++ fragcoalsim/test/test_frag.py
@@ -34,8 +34,31 @@ class FragmentationModelTestCase(unittest.TestCase):
                 effective_pop_size_of_ancestor = pop_size,
                 mutation_rate = mutation_rate,
                 migration_rate = 0.0)
+        self.assertEqual(fm.number_of_fragments, 2)
         pi_summary = stats.SampleSummarizer(
                 fm.sample_pi(200000))
+        self.assertAlmostEqual(expected_div, pi_summary.mean, places = 3)
+
+    def test_number_of_fragments(self):
+        gens_since_frag = 20000.0
+        pop_size = 10000
+        mutation_rate = 1e-6
+        expected_div = frag.get_expected_divergence_between_fragments(
+                generations_since_fragmentation = gens_since_frag,
+                effective_pop_size_of_ancestor = pop_size,
+                mutation_rate = mutation_rate)
+        fm = frag.FragmentationModel(
+                seed = 12345,
+                number_of_fragments = 10,
+                number_of_genomes_per_fragment = 1,
+                generations_since_fragmentation = gens_since_frag,
+                effective_pop_size_of_fragment = 1.0, # this shouldn't matter
+                effective_pop_size_of_ancestor = pop_size,
+                mutation_rate = mutation_rate,
+                migration_rate = 0.0)
+        self.assertEqual(fm.number_of_fragments, 10)
+        pi_summary = stats.SampleSummarizer(
+                fm.sample_pi(400000))
         self.assertAlmostEqual(expected_div, pi_summary.mean, places = 3)
 
 if __name__ == '__main__':
